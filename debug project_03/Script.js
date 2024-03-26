7754682b5209aa98ffe2ef922cf639953eac5924
Lugan2004 <visagielugan42@gmail.com>
2024-03-26 16:30:48 +0200

    added hasLoggedUser

--- debug project_03/Script.js
+++ debug project_03/Script.js
@@ -7,43 +7,37 @@ let calculated = '1'
 // 1. Fix the logCalc function to correctly increment the value of calculated.
 // 2. Ensure that the value is only logged to the console once after all calculations.
 // 3. Call logCalc function to calculate the value of calculated.
-// comment
 
-const logCalc = () => { 
+let hasLoggedUser = false; // Flag to track if the message has been logged
+
+const logCalc = () => {
   const isString = typeof calculated === 'string' && !isNaN(calculated)
   const calculatedAsNumber = isString ? parseInt(calculated) : calculated
-  calculated = calculatedAsNumber + 1 
-  console.log(`Calculated: ${calculated}`)
+  calculated = calculatedAsNumber + 1
 }
 
 const calcUser = () => {
-
   logCalc()
-
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
 const checkUser = () => {
-  if (user && state === 'requesting') {
-      console.log(`User: ${user} (${calculated})`)
+  if (user && state === 'requesting' && !hasLoggedUser) {
+    console.log(`User: ${user} (${calculated - 1})`)
+    hasLoggedUser = true; // Set the flag to true after logging the message
   }
 }
 
 // Only allowed to change code above
-
 checkUser()
 calcUser()
-
 checkUser()
 calcUser()
-
 checkUser()
 calcUser()
-
 checkUser()
 calcUser()
-
 checkUser()
 calcUser()
